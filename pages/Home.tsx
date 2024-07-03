c31b0b17b670d4d3979b0c35c16f808ae927a280
yovika1 <yovikaranga@gmail.com>
2024-07-04 00:59:17 +0530

    set icon

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -38,78 +38,73 @@ const Home = () => {
   }, []);
 
   return (
-    <div className=" h-[80vh] xl:h-[97vh] mt-20  bg-cover bg-center md:flex cursor-pointe">
+    <div className="h-[80vh] xl:h-[97vh] mt-20 bg-cover bg-center md:flex cursor-pointer">
       {showParticles && <ParticleApp />}
-      <div className="flex  w-full -mx-2 flex-wrap md:flex-nowrap item-center ">
+      <div className="flex w-full -mx-2 flex-wrap md:flex-nowrap items-center">
         <div
-          className={` left-0 mt-10 w-full h-full transition-transform duration-1000  ${
+          className={`left-0 mt-10 w-full h-full transition-transform duration-1000 ${
             loaded ? "translate-x-0" : "-translate-x-full"
           }`}
         >
-          <div className="px-8 ml-2  mt-14 md:mt-28 flex w-full md:h-full">
+          <div className="px-8 ml-2 mt-14 md:mt-28 flex w-full md:h-full">
             <div id="aboutMe">
-              <h1 className="text-white mb-6 text-[16px]">
-                WELCOME TO MY WORLD
-              </h1>
+              <h1 className="text-white mb-6 text-[16px]">WELCOME TO MY WORLD</h1>
               <div>
                 <h1 className="xl:text-[50px] lg:text-[40px] sm:text-[35px] text-[25px] font-semibold leading-3 mb-6 text-white">
                   Hi, I'M <span className="text-amber-500">Yovika</span>
                 </h1>
-                <div className=" md:w-[60vw] relative">
-                <AnimateText/>
+                <div className="md:w-[60vw] relative">
+                  <AnimateText />
                 </div>
-               
               </div>
               <div className="flex mt-6 -mx-2 justify-between w-80 h-8">
                 <Link href="/Contact">
-                  <button className="text hover:bg-purple-600 ">
-                    Hire ME Now
-                  </button>
+                  <button className="text hover:bg-purple-600">Hire ME Now</button>
                 </Link>
                 <a href="/Media/My-Resume.pdf" download="My_Resume.pdf">
-                  <button className="text hover:bg-purple-600">
-                    Download CV
-                  </button>
+                  <button className="text hover:bg-purple-600">Download CV</button>
                 </a>
               </div>
 
-              <div className="flex space-x-8  mt-11  ">
+              <div className="flex space-x-8 mt-11">
                 <div className="group relative">
-                  <a href="https://github.com/dashboard"><FaGithub className="text-white  text-3xl group-hover:text-fuchsia-700 transition duration-300" /></a>
-                  <div className="absolute  -top-1 w-full h-1 bg-fuchsia-700 blue-500 transition-all duration-300 group-hover:top-full group-hover:left-0"></div>
+                  <a href="https://github.com/dashboard">
+                    <FaGithub className="text-white text-3xl group-hover:text-fuchsia-700 transition duration-300" />
+                  </a>
+                  <div className="absolute -top-1 w-full h-1 bg-fuchsia-700 transition-all duration-300 group-hover:top-full group-hover:left-0"></div>
                 </div>
                 <div className="group relative">
-                  <a href="https://www.linkedin.com/in/yovika/"></a><FaLinkedin className="  text-white text-3xl group-hover:text-blue-800 transition duration-300" />
+                  <a href="https://www.linkedin.com/in/yovika/">
+                    <FaLinkedin className="text-white text-3xl group-hover:text-blue-800 transition duration-300" />
+                  </a>
                   <div className="absolute -top-1 w-full h-1 bg-blue-800 transition-all duration-300 group-hover:top-full group-hover:left-0"></div>
                 </div>
                 <div className="group relative">
                   <BiLogoGmail className="text-white -mt-1 text-4xl group-hover:text-red-600 transition duration-300" />
-                  <div className="absolute -top-1  w-full h-1 bg-red-600 transition-all duration-300 group-hover:top-full group-hover:left-0"></div>
+                  <div className="absolute -top-1 w-full h-1 bg-red-600 transition-all duration-300 group-hover:top-full group-hover:left-0"></div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="hidden md:block absolute   ml-[64vw] ">
+        <div className="hidden md:block absolute ml-[64vw]">
           <div className="group">
             <img
-                src="/Media/image 1.png"
+              src="/Media/image 1.png"
               className={`${
                 loaded ? "translate-x-0" : "translate-x-full"
-                } lg:w-[80vh] md:w-[60vw]  group-hover:animate-opacity-sequence`}
+              } lg:w-[80vh] md:w-[60vw] group-hover:animate-opacity-sequence`}
               style={{
                 borderBottomLeftRadius: "50%",
                 borderTopLeftRadius: "50%",
-                }}
+              }}
             />
           </div>
-                <div className=" sm:visible">
-                <IoIosArrowDown className="animated-icon text-4xl  " />
-
-                </div>
         </div>
       </div>
-
+      <div className="sm:visible absolute inset-x-0 bottom-0 flex justify-center">
+        <IoIosArrowDown className="animated-icon text-4xl" />
+      </div>
     </div>
   );
 };

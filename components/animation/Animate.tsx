f7789741a89fa9cdcb43e897ce69e73f4109b1de
yovika1 <yovikaranga@gmail.com>
2024-07-04 01:48:18 +0530

    Portfolio project

--- components/animation/Animate.tsx
+++ components/animation/Animate.tsx
@@ -4,11 +4,8 @@ export const AnimateText = () => {
   return (
     <TypeAnimation
     sequence={[
-      // Same substring at the start will only be typed out once, initially
       'A Full Stack Developer',
       1000, // wait 1s before replacing "Mice" with "Hamsters"
-      'A Mern stack developer',
-      1000,
       'A web developer',
       1000,
       'A Power BI Data Analyst',
@@ -22,27 +19,3 @@ export const AnimateText = () => {
 );
 };
   
-// import { motion } from 'framer-motion';
-
-// const AnimatedText = () => {
-//   return (
-//     <div className="container">
-//       <motion.h1
-//         initial={{ opacity: 0, y: 20 }}
-//         animate={{ opacity: 1, y: 0 }}
-//         transition={{ duration: 2 }}
-//       >
-//         Hi! I'm <span className="name">John Pater</span>
-//       </motion.h1>
-//       <motion.p
-//         initial={{ opacity: 0, y: 20 }}
-//         animate={{ opacity: 1, y: 0 }}
-//         transition={{ duration: 2, delay: 1 }}
-//       >
-//         And I'm <span className="role">UX/UI Developer</span>
-//       </motion.p>
-//     </div>
-//   );
-// };
-
-// export default AnimatedText;
